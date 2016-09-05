27d20a539ec96fafce859b6086dbe68283bad906
Nilay Jayswal <nilayjayswalnj@gmail.com>
2016-09-05 22:19:25 +0530

    about us content

--- about.php
+++ about.php
@@ -24,36 +24,30 @@ Template Name: About
 <section id="Section-1" class="fullbg" style="z-index:0;">
 <div class="container" style="box-shadow:none">
 <div class="row page-header text-center color-white">
-		<h1>About Us</h1>
+		<h1 style="color:white;font-family: 'PT Sans', sans-serif;">About Us</h1>
 		<hr style="border : 0;height: 1px; width:50%;background-image: linear-gradient(to right, rgba(255,255,255,255), rgba(255,255,255,255), rgba(255,255,255,255));">
 <br>
-		<p class="lead">
-			some lines....<br>some lines....<br>some lines....<br>some lines....
+		<p class="lead" style="color:white; font-family: 'Raleway', sans-serif;">
+			365 Online is the leading IT training and consultancy provider in the field of customer experience that focuses on quality, innovation, & speed. We utilized technology to bring results. We pride ourselves in great work ethic, integrity, and end-results. Our trainers and consultants are certified and highly experienced in their technology domain. 
 		</p>
 
 <br>
 <div class="row color-white">
 	<div class="col-md-12">
 		<div class="boxservice bottomb">
-			<i class="fa fa-building-o"></i>
-			<h3>Service 1</h3>
-			<p>
-				service description
-			</p>
+			<i class="fa fa-gear"></i>
+			<h3>Training Programs</h3>
+			
 		</div>
 		<div class="boxservice bottomb">
-			<i class="fa fa-laptop"></i>
-			<h3>Service 2</h3>
-			<p>
-				service description
-			</p>
+			<i class="fa fa-rocket"></i>
+			<h3>Project Consultancy</h3>
+			
 		</div>
 		<div class="boxservice rightb bottomb">
-			<i class="fa fa-gift"></i>
-			<h3>Service 3</h3>
-			<p>
-				service description 
-			</p>
+			<i class="fa fa-laptop"></i>
+			<h3>Digital Solutions </h3>
+			
 		</div>
 	</div>
 </div>
@@ -65,7 +59,7 @@ Template Name: About
 <div class="container" style="box-shadow:none">
 <div class="row page-header text-center">
 	
-		<h1>Customer Reviews</h1>
+		<h1 style="color:white;font-family: 'PT Sans', sans-serif;">Customer Reviews</h1>
  <hr style="border : 0;height: 1px; width:50%;background-image: linear-gradient(to right, rgba(255,255,255,255), rgba(255,255,255,255), rgba(255,255,255,255));">
 <br>
 <div class="row testimonials">
@@ -75,15 +69,16 @@ Template Name: About
 		<div class="testimonials-slider">
 			<div class="slide">
 				<div class="testimonials-carousel-thumbnail">
-					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/avatar.jpg">
-                                        <div class="testimonials-name">
-						 Bill Robinson <span>google.com</span>
-					</div>
+					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/1.jpg">    
 				</div>
 				<div class="testimonials-carousel-context">
+                                        <div class="testimonials-name">
+						 Amol Gavali<span>WebCenter Content/Imaging/Records Architect</span>
+                                                 <a href="https://www.linkedin.com/in/amol-gavali-a219201"><i class="fa fa-linkedin" style="color:black"></i></a>
+					</div>
 					<div class="testimonials-carousel-content">
-						<p>
-							some lines....<br>some lines....<br>some lines....
+						<p style="text-align:left;font-family: 'Raleway', sans-serif;">
+						365 Online, they are energetic, goal oriented individuals who can be counted on to deliver a high quality Webcenter Sites/Fatwire Training. They have a talent for understanding their audience and can tailor any presentation or training accordingly. If you want best in class Webcenter Sites Training, hire 365 online. 
 						</p>
 					</div>
 				</div>
@@ -91,22 +86,52 @@ Template Name: About
 			
 			<div class="slide">
 				<div class="testimonials-carousel-thumbnail">
-					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/avatar.jpg">
-                                        <div class="testimonials-name">
-						 Andrew Bilson <span>alpha.com</span>
-					</div>
+					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/2.jpg">     
 				</div>
 				<div class="testimonials-carousel-context">
+                                        <div class="testimonials-name">
+						 Ibrahim Maro<span>Senior Solution Consultant at Omnix Technology</span> <a href="https://www.linkedin.com/in/iamaro"><i class="fa fa-linkedin" style="color:black"></i></a>
+					</div>
 					<div class="testimonials-carousel-content">
-						<p>
-							some lines....<br>some lines....<br>some lines....
+						<p style="text-align:left;font-family: 'Raleway', sans-serif;">
+							365 online, they really knows their stuff. If you need to learn WebCenter Sites from experts, then 365 online is your best choice. With them, you get a comprehensive, advance, strong and within the budget training. I enjoyed their training and learned too much from them.
 						</p>
 					</div>
 				</div>
 			</div>
-		</div><br><br><br>
-<a href="#" class="buttoncolor"><i class="fa fa-link"></i>&nbsp;Write a review</a>
-<br><br>
+
+<div class="slide">
+				<div class="testimonials-carousel-thumbnail">
+					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/3.jpg">     
+				</div>
+				<div class="testimonials-carousel-context">
+                                        <div class="testimonials-name">
+						 Milena Djurasovic<span>Oracle UCM/Stellant Consultant</span> <a href="https://www.linkedin.com/in/mdjurasovic"><i class="fa fa-linkedin" style="color:black"></i></a>
+					</div>
+					<div class="testimonials-carousel-content">
+						<p style="text-align:left;font-family: 'Raleway', sans-serif;">
+							Very good training with excellent training material. 365 online have in-depth understanding of Wencenter Sites Product; they explain complex things in a simple wayand give you all you need to feel comfortable starting working with WebCenter Sites. They provide video of each class and their exercises material include everything you need to successfully complete each exercise.
+						</p>
+					</div>
+				</div>
+			</div>
+
+<div class="slide">
+				<div class="testimonials-carousel-thumbnail">
+					<img width="120" alt="" src="<?php bloginfo('template_directory'); ?>/assets/img/4.jpg">     
+				</div>
+				<div class="testimonials-carousel-context">
+                                        <div class="testimonials-name">
+						 Logixal Inc<span></span> <a href="http://www.logixal.com/"><i class="fa fa-globe" style="color:black"></i></a>
+					</div>
+					<div class="testimonials-carousel-content">
+						<p style="text-align:left;font-family: 'Raleway', sans-serif;">
+							365 online possess in depth knowledge of Webcenter Sites and very good industry experience. We are happy with the type of service they have provided to bring our project on track and also correcting the existing project design. Provided optimal solutions and advices to make our project as per the followed industry standards and structure.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div><br><br><br><br><br>
 	</div>
 </div>
 </div>
